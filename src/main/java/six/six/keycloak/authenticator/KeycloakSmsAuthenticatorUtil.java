7814a25f06f8a65bd8d3c8e802d3b42a1e6e0cac
Nick Pack <nick@nickpack.com>
2017-08-16 10:08:42 +0100

    Add string validation of UK phone numbers

--- src/main/java/six/six/keycloak/authenticator/KeycloakSmsAuthenticatorUtil.java
+++ src/main/java/six/six/keycloak/authenticator/KeycloakSmsAuthenticatorUtil.java
@@ -123,4 +123,8 @@ public class KeycloakSmsAuthenticatorUtil {
         long code = (long) (r.nextFloat() * maxValue);
         return Long.toString(code);
     }
+
+    public static boolean validateTelephoneNumber(String telephoneNumber) {
+        return telephoneNumber.matches("^(?:(?:\\(?(?:0(?:0|11)\\)?[\\s-]?\\(?|\\+)44\\)?[\\s-]?(?:\\(?0\\)?[\\s-]?)?)|(?:\\(?0))(?:(?:\\d{5}\\)?[\\s-]?\\d{4,5})|(?:\\d{4}\\)?[\\s-]?(?:\\d{5}|\\d{3}[\\s-]?\\d{3}))|(?:\\d{3}\\)?[\\s-]?\\d{3}[\\s-]?\\d{3,4})|(?:\\d{2}\\)?[\\s-]?\\d{4}[\\s-]?\\d{4}))(?:[\\s-]?(?:x|ext\\.?|\\#)\\d{3,4})?$");
+    }
 }
